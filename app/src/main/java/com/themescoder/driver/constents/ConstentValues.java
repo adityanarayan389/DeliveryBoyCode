9a18a42af3df80cd7d8673010ecf599a90a00f41
adityanarayan389 <adnarayan389@gmail.com>
2021-08-18 10:40:15 +0530

    Initial commit

---
+++ app/src/main/java/com/themescoder/driver/constents/ConstentValues.java
@@ -0,0 +1,9 @@
+package com.themescoder.driver.constents;
+
+public class ConstentValues {
+
+    public static final String WEBSITE_URL = "YOUR_WEBSITE_URL";
+    public static final String ECOMMERCE_CONSUMER_KEY = "YOUR_CONSUMER_KEY";
+    public static final String ECOMMERCE_CONSUMER_SECRET = "YOUR_CONSUMER_SECRET";
+
+}
